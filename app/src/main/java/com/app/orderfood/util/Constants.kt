a76c63bd2ad43c03865ee5b6a4101ad02047875a
yunusemresorkac <yunussemre36@gmail.com>
2024-01-05 20:16:02 +0300

    Update Constants.kt

--- app/src/main/java/com/app/orderfood/util/Constants.kt
+++ app/src/main/java/com/app/orderfood/util/Constants.kt
@@ -13,7 +13,7 @@ class Constants {
         const val COMPLETED_ORDER = 1
         const val PREPARING_ORDER = 0
         const val CANCELLED_ORDER = -1
-        const val FCM_KEY = "AAAA9y9tNa8:APA91bEk3QPOODvjAlEKRdb42P5NK-FZrykBWee4XSd2aJ7N8JV_Ccaung-P3OW4MHHmNdVyI0MdxHwP00a6cdgmZX3Hq_RUYpkoQDiB86WM9-AiEsTk1_KFVrzOcbYupvqT6ZNt4hww"
+        const val FCM_KEY = ""
 
 
     }
